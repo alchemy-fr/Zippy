37d90bf5d91ecade5b912bb15c49305c194b7d36
Nicolas Le Goff <legoff.n@gmail.com>
2013-01-03 16:04:38 +0100

    Add abstract Tests class

---
+++ tests/Alchemy/Zippy/Tests/AbstractTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Alchemy\Zippy\Tests;
+
+abstract class AbstractTest extends \PHPUnit_Framework_TestCase
+{
+    public static function getResourcesPath()
+    {
+        return __DIR__ . '/../../../resources';
+    }
+    
+}
