274a6ea6856f180ea5ac7984cbb0e52f39c8c002
Nicolas Le Goff <legoff.n@gmail.com>
2013-01-16 16:39:06 +0100

    Add zip output parser

--- src/Alchemy/Zippy/Parser/ZipOutputParser.php
+++ src/Alchemy/Zippy/Parser/ZipOutputParser.php
@@ -10,28 +10,88 @@
  */
 namespace Alchemy\Zippy\Parser;
 
+use Alchemy\Zippy\Member;
+
 /**
  * This class is responsable of parsing GNUTar command line output
  */
 class ZipOutputParser implements ParserInterface
 {
+    const LENGTH        = "(\d*)";
+    const ISO_DATE      = "([0-9]+-[0-9]+-[0-9]+\s+[0-9]+:[0-9]+)";
+    const FILENAME      = "(.*)";
+
     /**
      * @inheritdoc
      */
     public function parseFileListing($output)
     {
-        /**
-         * @todo
-         */
+        $lines = array_values(array_filter(explode("\n", $output)));
+        $members = array();
+
+        foreach ($lines as $line) {
+            $matches = array();
+
+            // 785  2012-10-24 10:39  file
+            if (!preg_match_all("#".
+                self::LENGTH    . "\s+" . // match (785)
+                self::ISO_DATE  . "\s+" . // match (2012-10-24 10:39)
+                self::FILENAME  .         // match (file)
+                "#",
+                $line, $matches, PREG_SET_ORDER
+            )) {
+                continue;
+            }
+
+            $chunks = array_shift($matches);
+
+            if (4 !== count($chunks)) {
+                continue;
+            }
+
+            $members[] = new Member(
+                $chunks[3],
+                $chunks[1],
+                \DateTime::createFromFormat("Y-m-d H:i", $chunks[2]),
+                '/' === substr($chunks[3], -1)
+            );
+        }
+
+        return $members;
     }
+
+      /**
+     * @inheritdoc
+     */
+    public function parseInflatorVersion($output)
+    {
+        $lines = array_values(array_filter(explode("\n", $output, 3)));
+
+        $chuncks = explode(' ', $lines[1], 5);
+
+        if (4 > count($chuncks)) {
+            return null;
+        }
+
+        return $chuncks[3];
+    }
+
 
     /**
      * @inheritdoc
      */
-    public function parseVersion($output)
+    public function parseDeflatorVersion($output)
     {
-        /**
-         * @todo
-         */
+        $lines = array_values(array_filter(explode("\n", $output, 2)));
+        $firstLine = array_shift($lines);
+        $chuncks = explode(' ', $firstLine, 3);
+
+        if (2 > count($chuncks)) {
+            return null;
+        }
+
+        list($name, $version) = $chuncks;
+
+        return $version;
     }
 }
