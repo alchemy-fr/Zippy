3b84cf329afe99d101958e6c1ab61e6b1700ec20
Romain Neutron <imprec@gmail.com>
2013-01-27 00:01:39 +0100

    Split ResourceMapper in TargetLocator, return ResourceCollection instead of array

--- src/Alchemy/Zippy/Resource/ResourceMapper.php
+++ src/Alchemy/Zippy/Resource/ResourceMapper.php
@@ -22,6 +22,13 @@ use Alchemy\Zippy\Resource\TeleporterFactory;
  */
 class ResourceMapper
 {
+    private $locator;
+
+    public function __construct(TargetLocator $locator)
+    {
+        $this->locator = $locator;
+    }
+
     /**
      * Maps resources to their appropriate ResourceTeleporter object
      *
@@ -29,197 +36,16 @@ class ResourceMapper
      */
     public function map($context, array $resources)
     {
-        // A set of resource that live in the context
-        $contextFiles = array();
-        // A set of resource that do not live in the context
-        $temporaryFiles = array();
-
+        $collection = new ResourceCollection($context);
 
         foreach ($resources as $location => $resource) {
-            $location = ltrim($location, '/');
-
-            // case resource is stream
-            // example fopen('http:// ...);
-            if (is_resource($resource)) {
-                $meta = stream_get_meta_data($resource);
-                $url = parse_url($meta['uri']);
-
-                if ($this->isLocalFilesystem($meta['wrapper_type'])
-                        && $this->isFileInContext($url['path'], $context)
-                            && !$this->isLocationCustomized($location)) {
-                    $target = $this->getRelativePathFromContext($url['path'], $context);
-
-                    $contextFiles[$target] = $this->getResourceTeleporter(
-                        $meta['wrapper_type'],
-                        $meta['uri'],
-                        $target
-                    );
-
-                    continue;
-                }
-
-                if (!$this->isLocationCustomized($location)) {
-                    $target = $this->getBaseName($meta['uri']);
-                } else {
-                    $target = $this->getCustomPath($location, $meta['uri']);
-                }
-
-                $temporaryFiles[$target] = $this->getResourceTeleporter(
-                    $meta['wrapper_type'],
-                    $meta['uri'],
-                    $target
-                );
-
-                continue;
-            }
-
-            // case resource is a resource URI or local path
-            // example  file:///path/to/local/file or http://path.to.remote/file
-            // or /path/to/local/file
-            if (is_string($resource)) {
-                $url = parse_url($resource);
-
-                // resource is a resource URI
-                if (isset($url['scheme'])) {
-                    if ($this->isLocalFilesystem($url['scheme'])
-                            && $this->isFileInContext($url['path'], $context)
-                                && !$this->isLocationCustomized($location)) {
-                        $target = $this->getRelativePathFromContext($url['path'], $context);
-
-                        $contextFiles[$target] = $this->getResourceTeleporter(
-                            $url['scheme'],
-                            $resource,
-                            $target
-                        );
-
-                        continue;
-                    }
-
-                    if (!$this->isLocationCustomized($location)) {
-                        $target = $this->getBaseName($resource);
-                    } else {
-                        $target = $this->getCustomPath($location, $resource);
-                    }
-
-                    $temporaryFiles[$target] = $this->getResourceTeleporter(
-                        $url['scheme'],
-                        $resource,
-                        $target
-                    );
-
-                    continue;
-                }
-
-                // resource is a local path
-                if ($this->isFileInContext($resource, $context)) {
-                    $target = $this->getRelativePathFromContext($resource, $context);
-                    $contextFiles[$target] = $this->getResourceTeleporter(
-                        'local',
-                        $resource,
-                        $target
-                    );
-                } else {
-                    $target = $this->getBaseName($resource);
-                    $temporaryFiles[$target] = $this->getResourceTeleporter(
-                        'local',
-                        $resource,
-                        $target
-                    );
-                }
+            if (!is_int($location)) {
+                $collection->add(new Resource($resource, $this->locator->locate($context, $resource)));
+            } else {
+                $collection->add(new Resource($resource, ltrim($location, '/')));
             }
         }
 
-        return array_merge($contextFiles, $temporaryFiles);
-    }
-
-    /**
-     * Get an instance of ResourceTeleporter
-     *
-     * @param String $protocole A protocole scheme
-     * @param String $uri       A resource URI
-     * @param String $target    A reosurce target location
-     *
-     * @return ResourceTeleporter
-     */
-    private function getResourceTeleporter($protocole, $uri, $target)
-    {
-        return new ResourceTeleporter(
-            TeleporterFactory::create($protocole),
-            $uri,
-            $target
-        );
-    }
-
-    /**
-     * Checks wheteher the path belong to the context
-     *
-     * @param String $path A resource path
-     *
-     * @return Boolean
-     */
-    private function isFileInContext($path, $context)
-    {
-        return false !== stripos($path, $context);
-    }
-
-    /**
-     * Gets the relative path from the context for the given path
-     *
-     * @param String $path A resource path
-     *
-     * @return String
-     */
-    private function getRelativePathFromContext($path, $context)
-    {
-        return ltrim(str_replace($context, '', $path), '/');
-    }
-
-    /**
-     * Gets the basename of a given path
-     *
-     * @param String $path A resource path
-     *
-     * @return String
-     */
-    private function getBaseName($path)
-    {
-        return basename($path);
-    }
-
-    /**
-     * Gets the full path for a given location and a given resource path
-     *
-     * @param String $location A path to the desired resource destination
-     * @param String $path     A path to a resource
-     *
-     * @return String
-     */
-    private function getCustomPath($location, $path)
-    {
-        return sprintf('%s/%s', ltrim(rtrim($location, '/'), '/'), $this->getBaseName($path));
-    }
-
-    /**
-     * Checks whether the given scheme can access the local filesystem
-     *
-     * @param String $scheme
-     *
-     * @return Boolean
-     */
-    private function isLocalFilesystem($scheme)
-    {
-        return 'plainfile' === $scheme || 'file' === $scheme;
-    }
-
-    /**
-     * Checks whether the given location is customized location
-     *
-     * @param String $location A desired location
-     *
-     * @return Boolean
-     */
-    private function isLocationCustomized($location)
-    {
-        return false === is_numeric($location);
+        return $collection;
     }
 }
