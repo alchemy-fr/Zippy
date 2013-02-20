c04d9c6254f3b57674ec6946a946d769bf41d499
mc <mc@192.168.1.22>
2013-02-20 04:45:48 +0000

    Changed to match current Interface methods signature.

--- src/Alchemy/Zippy/Adapter/ZipExtensionAdapter.php
+++ src/Alchemy/Zippy/Adapter/ZipExtensionAdapter.php
@@ -13,16 +13,19 @@ namespace Alchemy\Zippy\Adapter;
 
 use Alchemy\Zippy\Exception\RuntimeException;
 use Alchemy\Zippy\Exception\InvalidArgumentException;
+use Alchemy\Zippy\Exception\NotSupportedException;
 use Alchemy\Zippy\Archive\Member;
+use Alchemy\Zippy\Adapter\Resource\ResourceInterface;
+use Alchemy\Zippy\Adapter\Resource\ZipArchiveResource;
+use Alchemy\Zippy\Archive\Archive;
 
 /**
  * ZipExtensionAdapter allows you to create and extract files from archives using PHP Zip extension
  *
  * @see http://www.php.net/manual/en/book.zip.php
  */
-class ZipExtensionAdapter extends AbstractAdapter 
+class ZipExtensionAdapter extends AbstractAdapter
 {
-    private $zip;
     private $errorCodesMapping=array(
         \ZIPARCHIVE::ER_EXISTS =>"File already exists",
         \ZIPARCHIVE::ER_INCONS =>"Zip archive inconsistent",
@@ -34,7 +37,7 @@ class ZipExtensionAdapter extends AbstractAdapter
         \ZIPARCHIVE::ER_READ =>"Read error",
         \ZIPARCHIVE::ER_SEEK =>"Seek error"
     );
-    
+
     /**
      * Returns a new instance of the invoked adapter
      *
@@ -44,21 +47,24 @@ class ZipExtensionAdapter extends AbstractAdapter
      */
     public static function newInstance()
     {
-        try{
-            return new ZipExtensionAdapter();
-        }catch(RuntimeException $e){
-            throw $e;
-        }
+        return new ZipExtensionAdapter();
     }
-   
+
     public function __construct()
     {
-        if (!$this->isSupported()){
-            throw new RuntimeException("Zip Extension not available");
+        if (!$this->isSupported()) {
+            throw new RuntimeException("Zip Extension is not available");
         }
-        $this->zip=new \ZipArchive();
     }
- 
+
+    /**
+     * @inheritdoc
+     */
+    public function open($path)
+    {
+        return new Archive($path, $this, new ZipArchiveResource($this->_open($path)));
+    }
+
     /**
      * @inheritdoc
      */
@@ -66,212 +72,225 @@ class ZipExtensionAdapter extends AbstractAdapter
     {
          return class_exists("\\ZipArchive");
     }
-    
+
     /**
      * @inheritdoc
      */
-    public function listMembers($path)
+    public function listMembers(ResourceInterface $resource)
     {
-        $this->_open($path);
         $members = array();
-        for ($i=0;$i<$this->zip->numFiles;$i++){
-            $stat=$this->zip->statIndex($i);
+        for ($i=0;$i<$resource->getResource()->numFiles;$i++) {
+            $stat=$resource->getResource()->statIndex($i);
             $members[] = new Member(
-             $path,
+             $resource,
              $this,
              $stat['name'],
              $stat['size'],
-             new \DateTime('@'.$stat['mtime']), 
-             strlen($this->zip->getFromIndex($i,1))==0
+             new \DateTime('@'.$stat['mtime']),
+             strlen($resource->getResource()->getFromIndex($i,1))==0
             );
         }
-        $this->zip->close();
+
         return $members;
     }
-    
+
     /**
      * @inheritdoc
      */
     public static function getName()
     {
-        return 'zip';
+        return 'zip-extension';
     }
-    
+
     /**
      * @inheritdoc
      */
-    public function extract($path, $to = null)
+    public function extract(ResourceInterface $resource, $to = null)
     {
-        return $this->extractMembers($path, null, $to);
+        return $this->extractMembers($resource, null, $to);
     }
-   
+
     /**
      * @inheritdoc
      */
-    public function extractMembers($path, $members, $to = null)
+    public function extractMembers(ResourceInterface $resource, $members, $to = null)
     {
-        if (null === $to ) {
-            $to=dirname(realpath($path)); // if no destination is given, will extract to zip current folder
+        if (null === $to) {
+            $to=dirname(realpath($resource->getResource()->filename)); // if no destination is given, will extract to zip current folder
         }
-        if (!is_dir($to)){
+        if (!is_dir($to)) {
+            $resource->getResource()->close();
             throw new InvalidArgumentException(sprintf("%s is not a directory", $to));
         }
-        if (!is_writable($path)){
+        if (!is_writable($to)) {
+            $resource->getResource()->close();
             throw new InvalidArgumentException(sprintf("%s is not writable", $to));
         }
-       
-        $this->_open($path);
-        if (null!==$members){
+        if (null!==$members) {
            $membersTemp=(array) $members;
-           if (empty($membersTemp)){
-               $this->zip->close();
+           if (empty($membersTemp)) {
+               $resource->getResource()->close();
                throw new InvalidArgumentException("no members provided");
            }
            $members=array();
-           foreach ($membersTemp as $member){ // allows $members to be an array of strings or array of Members
-               if ($member instanceof Member){
+           foreach ($membersTemp as $member) { // allows $members to be an array of strings or array of Members
+               if ($member instanceof Member) {
                    $member=$member->getLocation();
                }
-               if ($this->zip->locateName($member)===false){
-                   $this->zip->close();
+               if ($resource->getResource()->locateName($member)===false) {
+                   $resource->getResource()->close();
                    throw new InvalidArgumentException(sprintf('%s is not on the zip file', $member));
                }
                $members[]=$member;
            }
         }
-       
-        if (!$this->zip->extractTo($to, $members)){
-            $this->zip->close();
-            throw new InvalidArgumentException($this->zip->getStatusString());
+
+        if (!$resource->getResource()->extractTo($to, $members)) {
+            $resource->getResource()->close();
+            throw new InvalidArgumentException($resource->getResource()->getStatusString());
         }
-        $this->zip->close();
+
         return new \SplFileInfo($to);
     }
-    
+
     /**
      * @inheritdoc
      */
-    public function remove($path, $files)
+    public function remove(ResourceInterface $resource, $files)
     {
-        $files=(array)$files;
-        if (empty($files)){
+        $files=(array) $files;
+        if (empty($files)) {
             throw new InvalidArgumentException("no files provided");
         }
-        $this->_open($path);
-        
-        // either extract all files or none in case of error
-        foreach ($files as $file){
-            if ($this->zip->locateName($file)===false){
-                $this->zip->unchangeAll();
-                $this->zip->close();
+
+        // either remove all files or none in case of error
+        foreach ($files as $file) {
+            if ($resource->getResource()->locateName($file)===false) {
+                $resource->getResource()->unchangeAll();
+                $resource->getResource()->close();
                 throw new InvalidArgumentException(sprintf('%s is not on the zip file', $file));
             }
-            if (!$this->zip->deleteName($file)){
-                $this->zip->unchangeAll();
-                $this->zip->close();
+            if (!$resource->getResource()->deleteName($file)) {
+                $resource->getResource()->unchangeAll();
+                $resource->getResource()->close();
                 throw new RuntimeException(sprintf('unable to delete %s', $file));
             }
         }
-        
-        $this->zip->close();
+        $this->flush($resource->getResource());
+
         return $files;
     }
-   
+
     /**
      * @inheritdoc
      */
-    public function add($path, $files, $recursive = true)
+    public function add(ResourceInterface $resource, $files, $recursive = true)
     {
-        $this->_open($path);
-        $this->addEntries($files,$recursive);
+        $files=(array) $files;
+        if (empty($files)) {
+            $resource->getResource()->close();
+            throw new InvalidArgumentException("no files provided");
+        }
+        $this->addEntries($resource,$files,$recursive);
+
         return $files;
     }
-    
+
     /**
      * @inheritdoc
      */
     public function create($path, $files = null, $recursive = true)
     {
-        $this->_open($path,\ZIPARCHIVE::CREATE);
-        $this->addEntries($files,$recursive);
-        return new Archive($path, $this);
+        $files=(array) $files;
+        if (empty($files)) {
+            throw new NotSupportedException("Cannot create an empty zip");
+        }
+        $resource=new ZipArchiveResource($this->_open($path,\ZIPARCHIVE::CREATE));
+        $this->addEntries($resource,$files,$recursive);
+
+        return new Archive($path, $this,$resource);
     }
-    
+
     // HELPER METHODS
     private function _open($path,$mode=\ZIPARCHIVE::CHECKCONS)
     {
-        $res=$this->zip->open($path,$mode);
+        $zip=new \ZipArchive();
+        $res=$zip->open($path,$mode);
         if ($res!==true) {
             throw new RuntimeException($this->errorCodesMapping[$res]);
         }
+
+        return $zip;
     }
-    
-    private function addEntries($files, $recursive)
+
+    private function addEntries(ZipArchiveResource $resource,array $files, $recursive)
     {
-        $files=(array)$files;
-        if (empty($files)){
-            throw new InvalidArgumentException("no files provided");
-        }
         $stack=new \SplStack();
-        
-        foreach ($files as $file){
-            $this->checkReadability($file);
-            if (is_dir($file)){
-                if ($recursive){
+
+        foreach ($files as $file) {
+            $this->checkReadability($resource->getResource(),$file);
+            if (is_dir($file)) {
+                if ($recursive) {
                     $stack->push($file.((substr($file,-1)==DIRECTORY_SEPARATOR)?'':DIRECTORY_SEPARATOR ));
-                }else{
-                    $this->addEmptyDir($file);
+                } else {
+                    $this->addEmptyDir($resource->getResource(),$file);
                 }
-            }else{
-                $this->addFileToZip($file);
+            } else {
+                $this->addFileToZip($resource->getResource(),$file);
             }
         }
-        
-        while(!$stack->isEmpty()){
+
+        while (!$stack->isEmpty()) {
             $dir=$stack->pop();
             $files=array_diff(scandir($dir),array(".","..")); // removes . and ..
-            if (count($files)>0){ 
-                foreach ($files as $file){
+            if (count($files)>0) {
+                foreach ($files as $file) {
                     $file=$dir.$file;
-                    $this->checkReadability($file);
-                    if (is_dir($file)){
+                    $this->checkReadability($resource->getResource(),$file);
+                    if (is_dir($file)) {
                         $stack->push($file.DIRECTORY_SEPARATOR);
-                    }else{
-                        $this->addFileToZip($file);
+                    } else {
+                        $this->addFileToZip($resource->getResource(),$file);
                     }
                 }
-            }else{
-                $this->addEmptyDir($dir);
+            } else {
+                $this->addEmptyDir($resource->getResource(),$dir);
             }
         }
-        $this->zip->close();
+        $this->flush($resource->getResource());
     }
-    
-    private function checkReadability($file)
+
+    private function checkReadability(\ZIPARCHIVE $zip,$file)
     {
-        if (!is_readable($file)){
-            $this->zip->unchangeAll();
-            $this->zip->close();
+        if (!is_readable($file)) {
+            $zip->unchangeAll();
+            $zip->close();
             throw new InvalidArgumentException(sprintf('could not read %s', $file));
         }
     }
 
-    private function addFileToZip($file)
+    private function addFileToZip(\ZIPARCHIVE $zip,$file)
     {
-        if (!$this->zip->addFile($file)){
-            $this->zip->unchangeAll();
-            $this->zip->close();
+        if (!$zip->addFile($file)) {
+            $zip->unchangeAll();
+            $zip->close();
             throw new RuntimeException(sprintf('unable to add %s to the zip file', $file));
         }
     }
 
-    private function addEmptyDir($dir)
+    private function addEmptyDir(\ZIPARCHIVE $zip,$dir)
     {
-        if (!$this->zip->addEmptyDir($dir)){
-            $this->zip->unchangeAll();
-            $this->zip->close();
+        if (!$zip->addEmptyDir($dir)) {
+            $zip->unchangeAll();
+            $zip->close();
             throw new RuntimeException(sprintf('unable to add %s to the zip file', $dir));
         }
     }
-    
+
+    private function flush(\ZIPARCHIVE $zip) // flush changes by reopening the file
+    {
+        $path=$zip->filename;
+        $zip->close();
+        $zip->open($path,\ZIPARCHIVE::CHECKCONS);
+    }
 }
