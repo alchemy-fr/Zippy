d860ea53c0ccf5fbc901aa885683849e18085cbf
Romain Neutron <imprec@gmail.com>
2013-03-01 14:49:19 +0100

    Add option to `TestCase::getResource`

--- tests/Alchemy/Zippy/Tests/TestCase.php
+++ tests/Alchemy/Zippy/Tests/TestCase.php
@@ -17,14 +17,16 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
             ->getMock();
     }
 
-    protected function getResource($data)
+    protected function getResource($data = null)
     {
         $resource = $this->getMock('Alchemy\Zippy\Adapter\Resource\ResourceInterface');
 
-        $resource->expects($this->any())
-            ->method('getResource')
-            ->will($this->returnValue($data));
-
+        if (null !== $data) {
+            $resource->expects($this->any())
+                ->method('getResource')
+                ->will($this->returnValue($data));
+        }
+        
         return $resource;
     }
 }
